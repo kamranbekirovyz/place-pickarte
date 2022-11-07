090bafafcf376d33f28298965c6fc1dfa01a0370
PORELARTE <kamranbekirovyz@gmail.com>
2022-11-07 18:09:28 +0400

    Camera update to the user's current location using geolocator plugin

--- lib/src/logic/artistic_place_picker_controller.dart
+++ lib/src/logic/artistic_place_picker_controller.dart
@@ -1,8 +1,14 @@
 import 'package:artistic_place_picker/artistic_place_picker.dart';
+import 'package:artistic_place_picker/src/enums/my_location_result.dart';
 import 'package:artistic_place_picker/src/logic/artistic_place_picker_bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ArtisticPlacePickerController {
   late final ArtisticPlacePickerBloc _bloc;
+  late final GoogleMapController? _googleMapController;
+
   ArtisticPlacePickerBloc get bloc => _bloc;
 
   ArtisticPlacePickerController(ArtisticPlacePickerConfig config) {
@@ -15,5 +21,68 @@ class ArtisticPlacePickerController {
 
   void close() {
     _bloc.close();
+  }
+
+  Future<MyLocationResult> goToMyLocation({
+    bool animate = true,
+    LocationAccuracy accuracy = LocationAccuracy.best,
+    double? zoom,
+    double tilt = 0.0,
+    double bearing = 0.0,
+  }) async {
+    zoom ??= _bloc.config.initialCameraPosition.zoom;
+
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      return MyLocationResult.serviceNotEnabled;
+    }
+
+    final permission = await Geolocator.checkPermission();
+    final hasValidPermission = [
+      LocationPermission.whileInUse,
+      LocationPermission.whileInUse,
+    ].contains(permission);
+
+    if (!hasValidPermission) {
+      switch (permission) {
+        case LocationPermission.denied:
+          return MyLocationResult.permissionDenied;
+        case LocationPermission.deniedForever:
+          return MyLocationResult.permissionDeniedForever;
+        case LocationPermission.unableToDetermine:
+        default:
+          return MyLocationResult.permissionUnableToDetermine;
+      }
+    }
+
+    final position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: accuracy,
+    );
+
+    final cameraUpdate = CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(
+          position.latitude,
+          position.longitude,
+        ),
+        zoom: zoom,
+        tilt: tilt,
+        bearing: bearing,
+      ),
+    );
+
+    animate
+        ? await _googleMapController?.animateCamera(
+            cameraUpdate,
+          )
+        : await _googleMapController?.moveCamera(
+            cameraUpdate,
+          );
+
+    return MyLocationResult.success;
+  }
+
+  @protected
+  void setGoogleMapController(GoogleMapController mapController) {
+    _googleMapController = mapController;
   }
 }
