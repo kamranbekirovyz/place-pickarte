c586d64185c853959de4534fe91b160d48a00133
PORELARTE <kamranbekirovyz@gmail.com>
2022-11-26 18:20:31 +0400

    Updated customized example app.

--- example/lib/modes/customized_place_picker.dart
+++ example/lib/modes/customized_place_picker.dart
@@ -1,4 +1,5 @@
 import 'package:example/modes/place_search_delegate.dart';
+import 'package:flutter_artkit/flutter_artkit.dart';
 import 'package:place_pickarte/place_pickarte.dart';
 import 'package:example/api_key.dart';
 import 'package:flutter/material.dart';
@@ -44,33 +45,56 @@ class _CustomizedPlacePickerState extends State<CustomizedPlacePicker> {
       body: PlacePickarteMap(_controller),
       bottomNavigationBar: _buildLocationDetails(),
       floatingActionButton: FloatingActionButton(
+        onPressed: _controller.goToMyLocation,
         child: const Icon(Icons.location_searching_outlined),
-        onPressed: () => _controller.goToMyLocation(animate: false),
       ),
     );
   }
 
   Widget _buildLocationDetails() {
-    return StreamBuilder<GeocodingResult?>(
-      stream: _controller.currentLocationStream,
-      builder: (context, snapshot) {
-        return SafeArea(
-          child: Card(
-            child: ListTile(
-              title: !snapshot.hasData
-                  ? const LinearProgressIndicator()
-                  : Text(
-                      '${snapshot.data?.formattedAddress.toString()}',
-                    ),
-            ),
+    return ElevatedBottomBar(
+      elevation: 8.0,
+      child: Column(
+        children: [
+          StreamBuilder<GeocodingResult?>(
+            stream: _controller.currentLocationStream,
+            builder: (context, snapshot) {
+              return ListTile(
+                leading: const Icon(Icons.maps_home_work_rounded),
+                title: !snapshot.hasData
+                    ? const LinearProgressIndicator()
+                    : Text(
+                        '${snapshot.data?.formattedAddress.toString()}',
+                      ),
+              );
+            },
           ),
-        );
-      },
+          Hoxy.h8(),
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              minimumSize: const Size(double.maxFinite, 56),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            onPressed: () => Navigator.of(context).pop(
+              _controller.currentLocation,
+            ),
+            icon: const Icon(
+              Icons.check_rounded,
+              color: Colors.white,
+            ),
+            label: const Text(
+              'Choose address',
+              style: TextStyle(color: Colors.white, fontSize: 16.0),
+            ),
+          ).padHorizontal(24.0),
+        ],
+      ),
     );
   }
 
   AppBar _buildAppBar() {
     return AppBar(
+      title: const Text('Customized Example'),
       actions: [
         IconButton(
           onPressed: () {
